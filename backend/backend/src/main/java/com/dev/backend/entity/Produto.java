3b9043639d10600fcd03b379ac75a6de64a2554d
costarafa <rafaelacs.rc@gmail.com>
2022-11-03 13:25:50 -0300

    relação produto/categoria

--- backend/backend/src/main/java/com/dev/backend/entity/Produto.java
+++ backend/backend/src/main/java/com/dev/backend/entity/Produto.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -28,4 +30,7 @@ public class Produto {
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
+    @ManyToOne
+    @JoinColumn(name="idCategoria")
+    private Marca marca;
 }
